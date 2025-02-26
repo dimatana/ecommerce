82c0b5ee938354d6d5d1d8833a07fb77c01a683d
dimatana <dima.tanasciuc@gmail.com>
2025-02-26 16:11:55 +0200

    fixed (swagger do not generate DTO's)

--- src/main/java/com/training/ecommerce/service/ProductService.java
+++ src/main/java/com/training/ecommerce/service/ProductService.java
@@ -7,7 +7,6 @@ import com.training.ecommerce.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
